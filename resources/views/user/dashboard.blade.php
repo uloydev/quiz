267561a23c495fb8be1c8b387cd0d2952501eecc
uloydev <uloydev@gmail.com>
2021-12-05 01:36:06 +0700

    [FIX] dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -22,28 +22,6 @@
                                 </div>
                             </div>
                         @endforeach
-                        @foreach ($availableQuiz as $quiz)
-                            <div class="col-md-4 p-2 h-auto">
-                                <div class="bg-custom-2 rounded-3 p-3 text-custom-3 shadow h-100">
-                                    <h4 class="text-custom-4">{{ $quiz->name }}</h4>
-                                    <hr>
-                                    <p>Waktu : {{ $quiz->time }} menit</p>
-                                    <p>Pertanyaan : {{ $quiz->questions_count }}</p>
-                                    <a class="btn bg-custom-4 text-custom-1 btn-sm " href="{{ route('user.attemp', ['quiz'=> $quiz->id]) }}">Attemp Now</a>
-                                </div>
-                            </div>
-                        @endforeach
-                        @foreach ($availableQuiz as $quiz)
-                            <div class="col-md-4 p-2 h-auto">
-                                <div class="bg-custom-2 rounded-3 p-3 text-custom-3 shadow h-100">
-                                    <h4 class="text-custom-4">{{ $quiz->name }}</h4>
-                                    <hr>
-                                    <p>Waktu : {{ $quiz->time }} menit</p>
-                                    <p>Pertanyaan : {{ $quiz->questions_count }}</p>
-                                    <a class="btn bg-custom-4 text-custom-1 btn-sm " href="{{ route('user.attemp', ['quiz'=> $quiz->id]) }}">Attemp Now</a>
-                                </div>
-                            </div>
-                        @endforeach
                     </div>
                 </div>
             </div>
